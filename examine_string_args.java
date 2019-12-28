358aa729c62d750a48c83d2e7bad6a9bf593c6c2
zhangweifeng66 <43534263+zhangweifeng66@users.noreply.github.com>
2019-12-28 14:56:54 +0800

    Add files via upload

---
+++ examine_string_args.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class examine_string_args {
+      public static void main(String[] args) {
+    	  System.out.println("arg is"+args[0]+" "+args[1]+" "+args[2]);
+      }
+}
