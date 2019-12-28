28e2254b29ddd297b419d726bb6d7e321c0c9d96
zhangweifeng66 <43534263+zhangweifeng66@users.noreply.github.com>
2019-12-28 15:15:24 +0800

    Add files via upload

---
+++ A_simple_Debug.java
@@ -0,0 +1,17 @@
+
+public class A_simple_Debug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int a=0;
+         int b=10;
+         b=a;
+         System.out.println("welcome "+b);
+         if(b==0) {
+        	 System.out.println("welcome b=0");
+         }else {
+        	 System.out.println("welcome b=10");
+         }
+	}
+
+}
